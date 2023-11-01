15958c407eb1256bf824087375b37efb6e9428ce
surenminas <surenminas@gmail.com>
2023-11-01 17:55:32 +0400

    create hub controller

---
+++ app/Http/Controllers/HubController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use function Psy\debug;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+// use App\Models\Post;
+
+class AboutController extends BaseController
+{
+    public function index()
+    {
+        return view('about');
+    }
+
+}
